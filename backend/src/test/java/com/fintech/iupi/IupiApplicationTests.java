63495d82a25e521ac0ec27779adafccc025f6dd3
Samir López <lopezs.dev@gmail.com>
2025-01-13 20:01:22 -0600

    commit inicial del backend, creacion y configuracion basica

---
+++ backend/src/test/java/com/fintech/iupi/IupiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.iupi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IupiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
