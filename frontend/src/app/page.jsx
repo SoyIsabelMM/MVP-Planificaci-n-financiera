73b85d799bd2e84c32628a1c90e632186b5ca895
angelchavez19 <infoangelchavez@gmail.com>
2025-01-16 16:43:20 -0500

    frontend: fix(index): initial size window

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,13 +1,13 @@
 "use client";
-import "./page.sass";
 
 import { useWindowSize } from "@/hooks/use-window-size.hook";
 import { IndexPageMobile } from "./components/mobile";
 import { IndexPageDesktop } from "./components/desktop";
 
 export default function Home() {
-  const { width } = useWindowSize();
+  const { width, height } = useWindowSize();
 
+  if (width === 0 && height === 0) return <></>;
   if (width < 768) return <IndexPageMobile />;
 
   return <IndexPageDesktop />;
