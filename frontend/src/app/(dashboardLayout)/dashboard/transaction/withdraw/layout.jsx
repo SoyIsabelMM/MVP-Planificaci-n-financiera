f6f9a972252fb17cd80607112c215d8080303d3e
angelchavez19 <infoangelchavez@gmail.com>
2025-01-29 18:42:14 -0500

    refactor(dashboard-layout)

--- frontend/src/app/(dashboardLayout)/dashboard/transaction/withdraw/layout.jsx
+++ frontend/src/app/(dashboardLayout)/dashboard/transaction/withdraw/layout.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
 import { useSearchParams } from "next/navigation";
 
 export default function TransferLayout({ children }) {
@@ -10,10 +9,7 @@ export default function TransferLayout({ children }) {
     : 1;
 
   return (
-    <DashboardLayout
-      title="Transferencia"
-      className="max-[840px]:w-full max-w-2xl px-4"
-    >
+    <div className="max-[840px]:w-full max-w-2xl px-4">
       <section className="flex w-full items-center gap-2 justify-end">
         <div className="h-1 bg-gray-200 rounded w-full">
           <div
@@ -24,6 +20,6 @@ export default function TransferLayout({ children }) {
         <span>({step}/3)</span>
       </section>
       {children}
-    </DashboardLayout>
+    </div>
   );
 }
--- frontend/src/components/DynamicBackground/DynamicBackground.jsx
+++ frontend/src/components/DynamicBackground/DynamicBackground.jsx
@@ -1,11 +1,24 @@
-'use client';
-import { usePathname } from 'next/navigation';
+"use client";
+import { usePathname } from "next/navigation";
+
+const backgroundColors = [
+  { path: "/", color: "#263238" },
+  { path: "/dashboard", color: "#FBF3FF", start: true },
+  { path: "/finding", color: "#FBF3FF" },
+];
 
 export default function DynamicBackground({ children }) {
   const pathname = usePathname();
 
-  const backgroundClass =
-    pathname === '/' ? 'bg-[#263238] text-white' : 'bg-white text-black';
+  const foundBackground = backgroundColors.find((bg) =>
+    bg.path === pathname || (bg.start && pathname.startsWith(bg.path))
+      ? true
+      : false
+  );
+
+  const backgroundClass = foundBackground
+    ? `bg-[${foundBackground.color}]`
+    : "bg-white text-black";
 
   return <div className={`${backgroundClass} min-h-screen`}>{children}</div>;
 }
