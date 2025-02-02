7bd69cd1c9937c21380f3512bb6eea6143372d31
SilvanaJ90 <silvanajaramillo90@gmail.com>
2025-02-02 11:04:45 -0500

    create columns dni

--- backend/iupi/users/models.py
+++ backend/iupi/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """ class User """
     email = models.EmailField(unique=True)
+    dni = models.CharField(max_length=20, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
