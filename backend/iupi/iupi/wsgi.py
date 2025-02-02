49c07e99e2c979abf1565977e4dfaf512d476f97
SilvanaJ90 <silvanajaramillo90@gmail.com>
2025-02-01 21:12:16 -0500

    update wsgi

--- backend/iupi/iupi/wsgi.py
+++ backend/iupi/iupi/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iupi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iupi.iupi.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
